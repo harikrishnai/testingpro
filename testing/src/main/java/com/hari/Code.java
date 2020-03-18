e132479fae2ad6b65bab7dac30d6a0568bcd6c9b
harikrishnai <iharikrishna180@gmail.com>
2020-03-18 21:47:51 +0530

    first commit

---
+++ testing/src/main/java/com/hari/Code.java
@@ -0,0 +1,9 @@
+package com.hari;
+
+public class Code {
+public static void main(String[] args) {
+	String v="welcome";
+	System.out.println(v);
+	System.out.println("hello");
+}
+}
---
+++ testing/src/main/java/com/hari/Test.java
@@ -0,0 +1,9 @@
+package com.hari;
+
+public class Test {
+public static void main(String[] args) {
+	System.out.println("hello");
+	System.out.println("welcome");
+	System.out.println("hari");
+}
+}
