61e77c211f82a4af80037472c5923381dcfd0e4e
harikrishnai <iharikrishna180@gmail.com>
2020-03-19 17:37:04 +0530

    afdsa

--- gittesting/src/main/java/com/gittesting/App.java
+++ gittesting/src/main/java/com/gittesting/App.java
@@ -3,6 +3,6 @@ package com.gittesting;
 public class App {
 public static void main(String[] args) {
 	System.out.println("welcome");
-	System.out.println("nandhi");
+	System.out.println("nandhi shiva");
 }
 }
