7682c183926f637621444d33dbc11b0ff65fb7c2
harikrishnai <iharikrishna180@gmail.com>
2020-03-19 17:36:03 +0530

    deleted

--- gittesting/src/main/java/com/gittesting/App.java
+++ gittesting/src/main/java/com/gittesting/App.java
@@ -3,7 +3,5 @@ package com.gittesting;
 public class App {
 public static void main(String[] args) {
 	System.out.println("welcome");
-	System.out.println("nandhi");
-	System.out.println("seshu");
 }
 }
