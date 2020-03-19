e0a0c2516001cd81d318ee323c9349cd5ab87177
harikrishnai <iharikrishna180@gmail.com>
2020-03-19 14:11:38 +0530

    delete two sys

--- gittesting/src/main/java/com/gittesting/App.java
+++ gittesting/src/main/java/com/gittesting/App.java
@@ -3,7 +3,6 @@ package com.gittesting;
 public class App {
 public static void main(String[] args) {
 	System.out.println("welcome");
-	System.out.println("hari");
-	System.out.println("rajesh");
+	
 }
 }
